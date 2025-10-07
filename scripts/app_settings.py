145ae19c85d8d08e1ddd852e6b77c46c966966c4
Drlinglong <80300206+Drlinglong@users.noreply.github.com>
2025-10-07 20:54:05 +1100

    V1.1.3
    version code - 1.1.3

--- scripts/app_settings.py
+++ scripts/app_settings.py
@@ -7,8 +7,8 @@ from scripts.config import prompts
 # --- 项目信息 ----------------------------------------------------
 PROJECT_NAME = "Paradox Mod 本地化工厂 - Paradox Mod Localization Factory"
 PROJECT_DISPLAY_NAME = "蕾姆丝计划 - Project Remis "
-VERSION = "1.1.0"
-LAST_UPDATE_DATE = "2025-10-06"
+VERSION = "1.1.3"
+LAST_UPDATE_DATE = "2025-10-07"
 COPYRIGHT = "© 2025 Project Remis Team"
 
 # --- 项目信息显示配置 --------------------------------------------
